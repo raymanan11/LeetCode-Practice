3a7164ccc8fa9cb02e9c79dc45aa19030e12e6e6
Raymond An <raymondan100@gmail.com>
2020-10-14 22:13:40 -0700

    Add instructions before each method as well as sample input and output for context about a problem, finished Product Array Except Self Problem

--- LeetCodePrep.playground/Contents.swift
+++ LeetCodePrep.playground/Contents.swift
@@ -1,4 +1,12 @@
 class Solution {
+    
+//    Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+//    You may assume that each input would have exactly one solution, and you may not use the same element twice.
+//    You can return the answer in any order.
+    
+//    Input: nums = [2,7,11,15], target = 9
+//    Output: [0,1]
+    
     func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
         var dict = [Int: Int]()
         var arr: [Int] = []
@@ -24,6 +32,13 @@ class Solution {
         return arr
     }
 
+//    Say you have an array for which the ith element is the price of a given stock on day i.
+//    If you were only permitted to complete at most one transaction (i.e., buy one and sell one share of the stock), design an algorithm to find the maximum profit.
+    
+//    Input: [7,1,5,3,6,4]
+//    Output: 5
+//    Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+//                 Not 7-1 = 6, as selling price needs to be larger than buying price.
     
     func maxProfit(_ prices: [Int]) -> Int {
         if prices.isEmpty {
@@ -44,6 +59,12 @@ class Solution {
         return maxProfit
     }
     
+//    Given an array of integers, find if the array contains any duplicates.
+//    Your function should return true if any value appears at least twice in the array, and it should return false if every element is distinct.
+    
+//    Input: [1,2,3,1]
+//    Output: true
+    
     func containsDuplicate(_ nums: [Int]) -> Bool {
         
         if nums.isEmpty { return false }
@@ -61,9 +82,43 @@ class Solution {
         }
         return false
     }
+    
+//    Given an array nums of n integers where n > 1,  return an array output such that output[i] is equal to the product of all the elements of nums except nums[i].
+    
+//    Input:  [4,5,1,8,2]
+//    Output: [80,64,320,40,160]
+    
+    func productExceptSelf(_ nums: [Int]) -> [Int] {
+            
+        var leftSideTotalMultiplacation = [Int]()
+        var rightSideTotalMultiplication = [Int]()
+        var resultsArr = [Int]()
+        
+        for index in 0..<nums.count {
+            rightSideTotalMultiplication.append(0)
+        }
+        
+        leftSideTotalMultiplacation.append(1)
+        for index in 1..<nums.count {
+            leftSideTotalMultiplacation.append(nums[index - 1] * leftSideTotalMultiplacation[index - 1])
+        }
+
+        rightSideTotalMultiplication[nums.count - 1] = 1
+        for index in stride(from: nums.count - 2, through: 0, by: -1) {
+            rightSideTotalMultiplication[index] = nums[index + 1] * rightSideTotalMultiplication[index + 1]
+        }
+
+        for index in 0..<nums.count {
+            resultsArr.append(leftSideTotalMultiplacation[index] * rightSideTotalMultiplication[index])
+        }
+        return resultsArr
+        
+    }
 
 }
 
 var solution = Solution()
+var arr = [4,5,1,8,2]
+solution.productExceptSelf(arr)
 
 
